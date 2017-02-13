47e1144e9911ab6f04fbada67312e77ce4e35e05
Doug Roeper <douglas.roeper@gmail.com>
2017-02-13 09:47:27 -0500

    - Fixed dependencies, used bazel-deps project

--- gen_deps.py
+++ gen_deps.py
@@ -11,26 +11,41 @@ workspace_start_file = script_dir + "/WORKSPACE_START"
 workspace_dep_file = script_dir + "/WORKSPACE_DEP"
 workspace_end_file = script_dir + "/WORKSPACE_END"
 
-# Remove old files
-command = "/bin/rm " + workspace_file + " " + build_file
 
-# Dependencies
-command += " && bazel run //src/tools/generate_workspace --"
-with open("deps.txt") as deps_file:
+def bazel_gen_workspace():
+  # Remove old files
+  command = "/bin/rm " + workspace_file + " " + build_file
+
+  # Dependencies
+  command += " && bazel run //src/tools/generate_workspace --"
+  with open("deps.txt") as deps_file:
+      for dep in deps_file:
+          if len(dep) > 1:
+              command += " --artifact=" + dep.rstrip()
+  # Specify where the generated files should go
+  command += " --output_dir=" + script_dir
+
+  # Workspace file generation
+  command += " && mv " + workspace_file + " " + workspace_dep_file
+  command += " && printf '# THIS FILE IS GENERATED DO NOT MODIFY\n\n' > " + workspace_file
+  command += " && cat " + workspace_start_file + " >> " + workspace_file
+  command += " && cat " + workspace_dep_file + " >> " + workspace_file
+  command += " && cat " + workspace_end_file + " >> " + workspace_file
+
+  # Delete dep file
+  command += " && /bin/rm " + workspace_dep_file
+
+  return command
+
+def bazel_deps_jar():
+  command = "java -jar target/bazel-deps-2.0-SNAPSHOT.jar "
+  with open("deps.txt") as deps_file:
     for dep in deps_file:
-        if len(dep) > 1:
-            command += " --artifact=" + dep.rstrip()
-# Specify where the generated files should go
-command += " --output_dir=" + script_dir
+      if len(dep) > 1:
+        command += " " + dep.rstrip()
+  return command
 
-# Workspace file generation
-command += " && mv " + workspace_file + " " + workspace_dep_file
-command += " && printf '# THIS FILE IS GENERATED DO NOT MODIFY\n\n' > " + workspace_file
-command += " && cat " + workspace_start_file + " >> " + workspace_file
-command += " && cat " + workspace_dep_file + " >> " + workspace_file
-command += " && cat " + workspace_end_file + " >> " + workspace_file
 
-# Delete dep file
-command += " && /bin/rm " + workspace_dep_file
-
-print(command)
+print("gen_workspace", bazel_gen_workspace())
+print("\n")
+print("bazel-deps", bazel_deps_jar())
--- src/test/java/edu/bu/vip/kinect/controllerv2/calibration/FragmenterTest.java
+++ src/test/java/edu/bu/vip/kinect/controllerv2/calibration/FragmenterTest.java
@@ -1,11 +1,52 @@
 package edu.bu.vip.kinect.controllerv2.calibration;
 
-import org.junit.Test;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
+import com.google.common.eventbus.EventBus;
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import edu.bu.vip.kinect.controller.calibration.Protos.CalibrationFrame;
+import edu.bu.vip.kinect.controllerv2.camera.FrameBus;
+import java.util.function.Consumer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class FragmenterTest {
+  private Injector injector;
+  private EventBus frameBus = new EventBus("test-frame-test");
+  private Fragmenter fragmenter;
+  @Mock
+  private Consumer<CalibrationFrame> frameConsumer;
+
+  @Before
+  public void setUp() throws Exception {
+    injector = Guice.createInjector(new AbstractModule() {
+      @Override
+      protected void configure() {
+        bind(EventBus.class).annotatedWith(FrameBus.class).toInstance(frameBus);
+        bind(CalibrationDataDB.class).toInstance(new InMemoryCalibrationDataDB());
+      }
+    });
+
+    fragmenter = injector.getInstance(Fragmenter.class);
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    fragmenter = null;
+    injector = null;
+  }
 
   @Test
-  public void test() {
-
+  public void noFramesTest() {
+    fragmenter.start(frameConsumer);
+    fragmenter.stop();
+    verifyNoMoreInteractions(frameConsumer);
   }
 }
